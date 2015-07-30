d8a2ea55b9de5537abd92e6459ef9fb93fc6b944
Eric Zhong <ericiszhongwenjia@qq.com>
2015-07-30 16:42:53 +0800

    dynamic table of homepage

--- mysite/vote/views.py
+++ mysite/vote/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User,Issue,Vote,Comment
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    issues = Issue.objects.all().order_by('-create_time')
+    items = []
+    for issue in issues:
+        item = {}
+        item['title'] = issue.title
+        item['user'] = issue.create_by.name
+        item['create_time'] =  issue.create_time.strftime('%Y-%m-%d %H:%M:%S');
+        item['reply'] =  Comment.objects.filter(issue_id=issue.id).count()
+        item['agree'] = Vote.objects.filter(issue_id=issue.id,agree=True).count()
+        item['disagree'] = Vote.objects.filter(issue_id=issue.id,agree=False).count()
+        items.append(item)
+
+    return render(request, 'index.html', {'items': items})
